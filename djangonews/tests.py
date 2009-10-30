8f495f73afd993bb7b83948fa4b1fb9cdf3a6ae3
f4nt <markr@imagescape.com>
2009-10-30 14:55:29 -0500

    test updates

--- djangonews/tests.py
+++ djangonews/tests.py
@@ -6,32 +6,31 @@ from djangonews import models
 
 class NewsTest(TestCase):
     
-    fixtures = ['djangonews/test_fixtures.json']
+    fixtures = ['djangonews/test_fixtures.json', 'test_fixtures', 'test_fixtures.json']
 
     def setUp(self):
         self.client = Client()
 
     def test_featured_article(self):
         response = self.client.get(
-            '/articles/2009/oct/30/featured-article/'
+            '/news/articles/2009/oct/30/featured-article/'
         )
-        print response.status_code
         self.assertEqual(response.status_code, 200)
 
     def test_normal_article(self):
         response = self.client.get(
-            '/articles/2009/oct/30/not-featured-article/'
+            '/news/articles/2009/oct/30/not-featured-article/'
         )
         self.assertEqual(response.status_code, 200)
 
     def test_article_listing(self):
-        response = self.client.get('/articles/')
+        response = self.client.get('/news/articles/')
         self.assertEqual(response.status_code, 200)
 
     def test_category_listing(self):
-        response = self.client.get('/categories/more-news/')
+        response = self.client.get('/news/categories/more-news/')
         self.assertEqual(response.status_code, 200)
 
     def test_main(self):
-        response = self.client.get('/')
+        response = self.client.get('/news/')
         self.assertEqual(response.status_code, 200)
